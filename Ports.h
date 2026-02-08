0f3842c65e4d186f320a7c5fdeb3bfde803a8454
ARM55 <debianmicro@gmail.com>
2026-02-08 11:58:50 +0000

    Update Ports.h

--- Ports.h
+++ Ports.h
@@ -1,10 +1,10 @@
 //This code is simply an experimental project//
 
-#define APN 5223
-#define SSH 22
-#define SMTP 25
-#define HTTP 80
-#define NETBIOS 138
+#define APN 5223 //Apple Push Notifications
+#define SSH 22 //SSH server for secure shell
+#define SMTP 25 // Simple Mail Transfer Protocol 
+#define HTTP 80 // Hypertext Transfer Protocol 
+#define NETBIOS 138 // NetBIOS Datagram Service
 #define LDAP 389
 #define SSL 443
 #define AFP 548
