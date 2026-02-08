ec87295b146422948d133f6f3903b970ad58b6c2
ARM55 <debianmicro@gmail.com>
2026-02-08 13:29:35 +0000

    Update Ports.h

--- Ports.h
+++ Ports.h
@@ -1,14 +1,16 @@
 //This code is simply an experimental project//
 
-#define APN 5223 //Apple Push Notifications
-#define SSH 22 //SSH server for secure shell
+#define APN 5223 // Apple Push Notifications
+#define SSH 22 // SSH server for secure shell
 #define SMTP 25 // Simple Mail Transfer Protocol 
 #define HTTP 80 // Hypertext Transfer Protocol 
 #define NETBIOS 138 // NetBIOS Datagram Service
 #define LDAP 389 // Lightweight Directory Access Protocol
 #define SSL 443 // Secure Sockets Layer  (HTTPS)
 #define AFP 548 // Apple Filing Protocol over TCP
-#define WC 500 //wifi calling
+#define WC 500 // wifi calling
+#define ASMTP 587 // Authenticated SMTP
+#define APNS 2197 //	Apple Push Notification Service
 
 //This library should only be used over a secure socket layer libaray like//
 //sys/socket.h with netinet/in.h//
